990d008df4b5cc2135464bde04423c51274429a2
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-09 10:17:11 +0330

    email validate

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const form = document.querySelector("form"),
     emailField = form.querySelector(".email-field"),
-    emailInput=emailField.querySelector(".email "),
-    passField=form.querySelector(".pass-field"),
-    pass=passField.querySelector(".pass"),
-     pass2Field=form.querySelector(".pass2-field"),
-    pass2=passField.querySelector(".pass2");
+    emailInput = emailField.querySelector(".email "),
+    passField = form.querySelector(".pass-field"),
+    passInput = passField.querySelector(".pass"),
+    pass2Field = form.querySelector(".pass2-field"),
+    pass2Input = passField.querySelector(".pass2");
 
 
-    
+//email validation
+function checkEmail() {
+    const emailPattern = /^[^]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailInput.value.match(emailPattern)) {
+        return emailField.classList.add("invalid")
+    }
+    return emailField.classList.remove("invalid");
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    checkEmail()
+    emailInput.addEventListener("keyup",checkEmail)
+})
