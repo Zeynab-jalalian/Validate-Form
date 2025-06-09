22c20496a4d96074f840aa00262f86ac322109d0
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-09 10:38:38 +0330

    //show and hide Password

--- script.js
+++ script.js
@@ -9,15 +9,28 @@ const form = document.querySelector("form"),
 
 //email validation
 function checkEmail() {
-    const emailPattern = /^[^]+@[^ ]+\.[a-z]{2,3}$/;
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
     if (!emailInput.value.match(emailPattern)) {
         return emailField.classList.add("invalid")
     }
     return emailField.classList.remove("invalid");
 }
-
+//submit form
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     checkEmail()
-    emailInput.addEventListener("keyup",checkEmail)
+    emailInput.addEventListener("keyup", checkEmail)
+})
+//show and hide Password
+const eyeIcons = document.querySelectorAll(".show-hide");
+eyeIcons.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        const pInput = eyeIcon.parentElement.querySelector("input");
+        if (pInput.type === "password") {
+            eyeIcon.classList.replace("ri-eye-off-fill", "ri-eye-fill");
+            return pInput.type="text";
+        }
+          eyeIcon.classList.replace("ri-eye-fill", "ri-eye-off-fill");
+            return pInput.type="password";
+    })
 })
