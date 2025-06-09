d063f7201727fb090d0665bfdddd4162fc396c16
Zeynab Jalalian <zjalalianweb@gmail.com>
2025-06-09 10:01:57 +0330

    start js part DOM

---
+++ script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("form"),
+    emailField = form.querySelector(".email-field"),
+    emailInput=emailField.querySelector(".email "),
+    passField=form.querySelector(".pass-field"),
+    pass=passField.querySelector(".pass"),
+     pass2Field=form.querySelector(".pass2-field"),
+    pass2=passField.querySelector(".pass2");
+
+
+    
